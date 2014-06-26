c1e6cc38d930725af08e55f882db30383371bd37
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2014-06-26 11:53:49 +0200

    Improved the text utils to be much more robust and handle classes better.

--- xylem/text.py
+++ xylem/text.py
@@ -17,18 +17,48 @@ def to_str(obj, encoding='utf-8', errors='replace'):
     if isinstance(obj, six.text_type):
         return obj
     elif isinstance(obj, six.binary_type):
-        return obj.decode(encoding=encoding, errors=errors)
+        return obj.decode(encoding, errors)
+    elif isinstance(obj, type):
+        return to_str(obj.__name__)
     else:
-        # TODO: possibly add some error handling here if needed
-        return six.text_type(obj, encoding=encoding, errors=errors)
+        value = None
+        if six.PY2:
+            try:
+                value = obj.__unicode__()
+            except (AttributeError, UnicodeError, TypeError):
+                pass
+        if value is None:
+            try:
+                value = str(obj)
+            except UnicodeError:
+                try:
+                    value = obj.__str__()
+                except (AttributeError, UnicodeError):
+                    value = ''
+        if not isinstance(value, six.text_type):
+            value = six.text_type(value, encoding, errors)
+        return value
 
 
 def to_bytes(obj, encoding='utf-8', errors='replace'):
     """Helper for converting to encoded byte-strings in py2 and py3."""
     if isinstance(obj, six.text_type):
-        return obj.encode(encoding=encoding, errors=errors)
+        return obj.encode(encoding, errors)
     if isinstance(obj, six.binary_type):
         return obj
     else:
-        # TODO: possible add some error handling here if needed
-        return six.binary_type(obj, encoding=encoding, errors=errors)
+        try:
+            value = str(obj)
+        except UnicodeError:
+            try:
+                value = obj.__str__()
+            except (AttributeError, UnicodeError):
+                value = ''
+                if six.PY2:
+                    try:
+                        value = obj.__unicode__()
+                    except (AttributeError, UnicodeError):
+                        pass
+        if isinstance(value, six.text_type):
+            value = value.encode(encoding, errors)
+        return value
