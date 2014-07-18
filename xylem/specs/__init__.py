6b89bf340c7afd6f3fff680287e9f2156fe6cfdc
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2014-07-19 01:55:14 +0200

    Fix type causing import error.

--- xylem/specs/__init__.py
+++ xylem/specs/__init__.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from .impl import verify_spec_name
 from .impl import get_spec_plugin_list
 from .impl import Spec
-from .rules import SpecParsingError
+from .plugins.rules import SpecParsingError
 
 __all__ = ['get_spec_plugin_list', 'SpecParsingError', 'verify_spec_name',
            'Spec']
