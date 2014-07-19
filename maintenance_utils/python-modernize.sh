c9147b35dc828fd5310db2145ccdc132383a4916
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2014-07-19 16:51:20 +0200

    Add comments to maintenace utils.

--- maintenance_utils/python-modernize.sh
+++ maintenance_utils/python-modernize.sh
@@ -2,4 +2,6 @@
 # Install development version from this fork:
 # https://github.com/takluyver/python-modernize
 # Seems to be more maintained
+# Run in root of xylem repository to update python files in such a way
+# as to make maintaining portable py2/py3 code easier.
 python-modernize xylem test --future-unicode
